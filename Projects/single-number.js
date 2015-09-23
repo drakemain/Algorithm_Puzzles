5be9f580c031db8e228ed8088ad6656d679b13cc
drakemain <demain1990@gmail.com>
2015-09-22 19:07:49 -0700

    single number finder

---
+++ Projects/single-number.js
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/single-number/
+
+var testArr = [8, 1, 9, 5, 3, 11, 2, 2, 4, 6, 10, 3, 1, 5, 6, 11, 4, 7, 9, 10, 8];
+
+var singleNumber = function(nums) {
+  var numCounter = {};
+
+  for (var i in nums) {
+    if (!numCounter[nums[i]]) {
+      numCounter[nums[i]] = 1;
+    } else {
+      numCounter[nums[i]]++;
+    }
+  }
+
+  for (var j in numCounter) {
+    if (numCounter[j] === 1) {return j;}
+  }
+};
+
+console.log(singleNumber(testArr));
