13d4d3088470643aa0e88e86341ba2561c468a35
drakemain <demain1990@gmail.com>
2015-10-25 15:08:35 -0700

    comments

--- Projects/add-digits.js
+++ Projects/add-digits.js
@@ -1,5 +1,7 @@
 //http://leetcode.com/problems/add-digits/
+//add the digits of an int repeatedly until the result is one digit
 
+//num a non-negative integer
 var addDigits = function(num) {
   var sum = num;
   var iterator = 0;
@@ -7,14 +9,18 @@ var addDigits = function(num) {
   while (true){
     console.log("Iteration: " + iterator);
 
+    //checks to see if sum has 1 digit
     if (sum.toString().length === 1){
       return sum;
     }
 
+    //calls digitSplitter, which returns an array of the digits of sum
     var digitArray = digitSplitter(sum);
 
+    //ensures sum is 0 for each pass
     sum = 0;
 
+    //adds the values of the digits together and stores value in sum
     for (var i in digitArray) {
       sum += digitArray[i];
     }
